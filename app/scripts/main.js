9d8f47f96e07c8dc729e3626186d70e118d6ea5e
Vincent <vp@us-elitegear.com>
2015-02-25 01:49:25 -0500

    initial commit

---
+++ app/scripts/main.js
@@ -0,0 +1,7 @@
+'use strict';
+
+$(document).ready(function () {
+	$(function () {
+		$('.player').mb_YTPlayer(); // .player - class to add for playing video ( see the div above to understand)
+	});
+});
