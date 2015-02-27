b86f03d09efea89ef13c03ac4b67a4625300ef20
Vincent Pantano <kingbobiv@gmail.com>
2015-02-27 03:00:05 -0500

    multiple videos demo

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -3,5 +3,7 @@
 $(document).ready(function () {
 	$(function () {
 		$('.player').mb_YTPlayer(); // .player - class to add for playing video ( see the div above to understand)
+		$('.player2').mb_YTPlayer(); // .player - class to add for playing video ( see the div above to understand)
+		$('.player3').mb_YTPlayer(); // .player - class to add for playing video ( see the div above to understand)
 	});
 });
--- dist/scripts/7fcd77e2.main.js
+++
@@ -1,1 +0,0 @@
-"use strict";$(document).ready(function(){$(function(){$(".player").mb_YTPlayer()})});
